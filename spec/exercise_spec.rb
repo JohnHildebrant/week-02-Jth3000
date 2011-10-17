bcd7d3088c7817220ac91a955771bd082549a006
John Hildebrant <john.hildebrant@gmail.com>
2011-10-17 11:28:18 -0700

    Test fais and you're my only hope.

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -2,7 +2,7 @@ describe "Integers" do
   context "when calling even?" do
     it "should return true if the integer is even or false if the integer is odd" do
       num = 23
-      num.even?.should eq false
+      num.even?.should eq true
     end
   end
 end
